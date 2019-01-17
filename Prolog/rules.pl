ef20b9efde9a8e50e29b52fad2251ef7ccf26f36
Andreas Kontogiannis <el15187@central.ntua.gr>
2019-01-17 20:33:02 +0000

    Update rules.pl

--- Prolog/rules.pl
+++ Prolog/rules.pl
@@ -1,60 +1,8 @@
-
 :-include('next.pl').
-/*
-:-include('closure.pl').
-:-include('lineLanes.pl').
-:-include('lineTraffic.pl').
-*/
 :-include('nodeF.pl').
 :-include('node.pl').
 
-
 /*Fy + Gy - Fx + Gx */
-
-/*
-getG_from_path([], _) :- !.
-getG_from_path([[_, G]|_], G) :- !.
-
-getFirstFront([], _, _).
-getFirstFront([[ID, F]|_], ID, F) :- !.
-
-addFront(X, [], [X]) :- !.
-addFront([ID, F], [[Id1, F1], [Id2, F2]], NewFront) :-
-	( F > F2 -> NewFront = [[Id1, F1], [Id2, F2], [ID, F]]
-	; 
-	  F < F1 -> NewFront = [[ID, F], [Id1, F1], [Id2, F2]] 
-	;
-	  F =< F2 -> NewFront = [[Id1, F1], [ID, F], [ID, F2]]
-	), !.  
-addFront([ID, F], [[Id1, F1]|Front], NewFront):-
-	( F < F1 -> NewFront = [[ID, F], [Id1, F1]|Front]
-	;
-	  F = F1, ID \= Id1 -> NewFront = [[Id1, F1], [ID, F]|Front]
-	; 
-	  F > F1 -> addFront([ID, F], Front, NF),
-	            NewFront = [[Id1, F1]|NF]
-	), !.
-
-searchFront(_, [], [], yes) :- !.
-searchFront([ID, F], [[Id1, F1]|Front], NewFront, Equal) :-
-	( ID =:= Id1, F < F1 -> Equal = yes, NewFront = Front
-	;
-	  ID =:= Id1, F >= F1 -> Equal = no, NewFront = [[Id1, F1]|Front]
-	;
-	  searchFront([ID, F], Front, NF, Equal),
-	  NewFront = [[Id1, F1]|NF]
-	), !.
-
-
-removeFront([_|Xs], Xs).
-
-getF([_, F], F).
-
-getID([Id, _], Id).
-
-addClosure(X, Closure, [X|Closure]).  	
-
-*/
 
 findF(X, F) :-
 	nodeF(X, F), !.
